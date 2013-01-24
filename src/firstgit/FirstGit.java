9fc88196916833fcf381e5c0de947a515f4f86d2
dbond4@my.wctc.edu <>
2013-01-24 17:23:47 -0600

    added doctor who comment

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -12,5 +12,8 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("I'm not going to say hello to the Universe here.");
+        System.out.println("I'm going to say Allons-y");
+        System.out.println("That's a Doctor Who reference, in case you were wondering.");
     }
 }

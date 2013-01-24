41708bd4feba2ab533bbed1751b8620717164208
dbond4@my.wctc.edu <>
2013-01-24 17:18:24 -0600

    Added more files, println

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -12,6 +12,8 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println();
+        System.out.println("Hello Universe");
+        System.out.println("Goodbye!");
+        System.out.println("Kidding, I'm back");
     }
 }
